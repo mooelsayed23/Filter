7009c3bee6203203ff7d2da598bd3a809374738f
ahmed <ahmed.mooelsayed@gmail.com>
2024-04-09 14:07:41 +0200

    v2 aside updata

--- src/component/sidbar/Sidbar.jsx
+++ src/component/sidbar/Sidbar.jsx
@@ -29,62 +29,68 @@ const Sidbar = () => {
 
   return (
     <>
-    <div className="border border-top-0 border-bottom-0  pt-3 pe-5 ps-4 sidbar-contaner fixed" >
-      <h5>Category</h5>
-      <div className="form-check my-2">
-        <div>
-          <input
-            className="form-check-input"
-            type="radio"
-            name="category"
-            id="allCat"
-            onChange={() => dispatch(setCategory(""))}
-            checked={cate === ""}
-          />
+      <div className="border-r sidbar-contaner fixed h-screen overflow-y-auto pb-5 text-sm">
+        <div className="bg-white w-4/5 mx-auto my-4 rounded-tr-xl p-3.5">
+          <h5>Category</h5>
+          <div className="form-check my-2">
+            <div>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="category"
+                id="allCat"
+                onChange={() => dispatch(setCategory(""))}
+                checked={cate === ""}
+              />
 
-          <label className="form-check-label" htmlFor="allCat">
-            All
-          </label>
+              <label className="form-check-label" htmlFor="allCat">
+                All
+              </label>
+            </div>
+            {generateItems("category")}
+          </div>
         </div>
-        {generateItems("category")}
-      </div>
-      <b>Price</b>
-      <div className="form-check my-2 ">
-        {Object.entries(priceRangeMap).map(([id, labelText]) => (
-          <Fragment key={id}>
-            <input
-              className="form-check-input"
-              type="radio"
-              name="price"
-              id={id}
-              onChange={(event) => dispatch(setPrice(event.target.id))}
-              checked={price === id}
-            />
-            <label className="form-check-label" htmlFor={id}>
-              {labelText}
-            </label>
-            <br />
-          </Fragment>
-        ))}
-      </div>
-      <b>Color</b>
-      <div className="form-check my-2">
-        <div>
-          <input
-            className="form-check-input"
-            type="radio"
-            name="color"
-            id="allColor"
-            onChange={() => dispatch(setColor(""))}
-            checked={color === ""}
-          />
-          <label className="form-check-label" htmlFor="allColor">
-            All
-          </label>
+        <div className="bg-white w-4/5 mx-auto my-4 rounded-tr-xl p-3.5">
+          <h5>Price</h5>
+          <div className="form-check my-2 ">
+            {Object.entries(priceRangeMap).map(([id, labelText]) => (
+              <Fragment key={id}>
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="price"
+                  id={id}
+                  onChange={(event) => dispatch(setPrice(event.target.id))}
+                  checked={price === id}
+                />
+                <label className="form-check-label" htmlFor={id}>
+                  {labelText}
+                </label>
+                <br />
+              </Fragment>
+            ))}
+          </div>
         </div>
-        {generateItems("color")}
+        <div className="bg-white w-4/5 mx-auto my-4 rounded-tr-xl p-3.5">
+          <h5>Color</h5>
+          <div className="form-check my-2">
+            <div>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="color"
+                id="allColor"
+                onChange={() => dispatch(setColor(""))}
+                checked={color === ""}
+              />
+              <label className="form-check-label" htmlFor="allColor">
+                All
+              </label>
+            </div>
+            {generateItems("color")}
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 };
